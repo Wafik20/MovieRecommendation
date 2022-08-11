eafbf5bf16bf469b97da39aca2cd3687dbe2d562
Wafik20 <wafik.wt@gmail.com>
2022-08-11 14:14:11 -0400

    added minor changes to the gui

--- movie_rec2.py
+++ movie_rec2.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from sre_parse import State
 from ttkwidgets.autocomplete import AutocompleteCombobox
 from tkinter import *
 import logic
@@ -8,7 +10,7 @@ countries = logic.titles
 
 ws = Tk()
 ws.title('Movie Recommendations')
-ws.geometry('500x450')
+ws.geometry('500x500')
 ws.config(bg='#8DBF5A')
 
 frame = Frame(ws, bg='#8DBF5A')
@@ -17,7 +19,7 @@ frame.pack(expand=False)
 Label(
     frame, 
     bg='#8DBF5A',
-    font = ('Times',21),
+    font ='Georgia 20 italic bold',
     text='Enter Movie : '
     ).pack(side=TOP)
     
@@ -28,6 +30,7 @@ entry = AutocompleteCombobox(
     completevalues=countries
     )
 def print_rec():
+    text_box.config(state = NORMAL)
     text_box.delete("1.0","end")
     movie = entry.get()
     recommendation = logic.get_recommendations(movie)
@@ -36,10 +39,11 @@ def print_rec():
        titles += (str(i+1) + '-' +recommendation[i])
        titles += '\n'
     text_box.insert(END,titles)
+    text_box.config(state = DISABLED)
 button = Button(ws, text="Get movies recommendations", command=print_rec)
 entry.pack(side=TOP)
 button.place(in_ = entry, relx = 0.2, rely = 1.2)
-text_box = Text(ws, height = 20, width = 60)
-text_box.place(in_=entry, relx = -0.425, rely = 2.0)
+text_box = Text(ws, height = 15, width = 30,font='Georgia 14 italic bold',relief='sunken', highlightthickness=1, borderwidth=2)
+text_box.place(in_=ws, relx = 0.1, rely = 0.23)
 ws.resizable(False,False)
 ws.mainloop()   
